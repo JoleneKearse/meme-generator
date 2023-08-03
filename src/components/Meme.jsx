ba01b9a06480a52ee18b34a6dee18ee9dea8236c
Jolene Kearse <pharaohnutz@gmail.com>
2023-08-03 16:24:57 -0400

    learning: add memesData file to follow lesson & hook up

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react"
 import meme from "/meme.png"
+import memesData from "../memesData.js"
 
 const Meme = () => {
+  const [memeObj, setMemeObj] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: ""
+  })
+  const [allMemeImages, setAllMemeImages] = useState(memesData)
+
+  function getMemeImage(e) {
+    e.preventDefault()
+    const memesArray = allMemeImages.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    setMemeObj(prevMeme => ({
+      ...prevMeme,
+      randomImage: memesArray[randomNumber].url
+    }))
+  }
+
   return (
     <>
       <form className="form-ele">
@@ -21,12 +40,15 @@ const Meme = () => {
           />
         </div>
         <button
-          type="submit" className="flex-gap get-meme"
+          type="submit" className="flex-gap get-meme" onClick={getMemeImage}
         >
           <p className="btn-text">Get a new meme image</p>
           <img src={meme} className="icon" alt="" />
         </button>
       </form>
+      <div className="meme-output">
+        {memeObj.randomImage && <img src={memeObj.randomImage} className="meme" alt="" />}
+      </div>
     </>
   )
 }
